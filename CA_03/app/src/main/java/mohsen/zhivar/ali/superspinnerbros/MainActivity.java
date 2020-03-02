c4868b247a577334e23893b14eae99160c517e9d
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-03-02 17:24:23 +0330

    Create Game Activity

--- CA_03/app/src/main/java/mohsen/zhivar/ali/superspinnerbros/MainActivity.java
+++ CA_03/app/src/main/java/mohsen/zhivar/ali/superspinnerbros/MainActivity.java
@@ -2,7 +2,10 @@ package mohsen.zhivar.ali.superspinnerbros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                try {
+                    Intent k = new Intent(MainActivity.this, GameActivity.class);
+                    startActivity(k);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+
 }
