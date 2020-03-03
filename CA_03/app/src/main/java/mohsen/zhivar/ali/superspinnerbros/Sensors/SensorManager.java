afd761f09a2d7ff67e48c88845d7930125c9c002
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-03-03 17:20:05 +0330

    Create SensorManager

---
+++ CA_03/app/src/main/java/mohsen/zhivar/ali/superspinnerbros/Sensors/SensorManager.java
@@ -0,0 +1,23 @@
+package mohsen.zhivar.ali.superspinnerbros.Sensors;
+
+public class SensorManager {
+
+    public SensorManager(String sensorType){
+        // TODO
+    }
+
+    double getAngleX(){
+        // TODO
+        return 0;
+    }
+
+    double getAngleZ(){
+        // TODO
+        return 0;
+    }
+
+    double getNForce(){
+        // TODO
+        return 0;
+    }
+}
