b258ab59f58a85d08971d73528d6d55e9d331a5f
zhivar <zhivarsourati@gmail.com>
2020-04-18 17:08:59 +0430

    Add paint and selectFromGallery activity

--- frontsample/app/src/main/java/com/example/front_sample/MainActivity.java
+++ frontsample/app/src/main/java/com/example/front_sample/MainActivity.java
@@ -26,7 +26,7 @@ import java.net.URI;
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    public static final int IMAGE_GALLERY_REQUEST = 20;
+
 
 
     @Override
@@ -36,75 +36,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onSend(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-
-        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        String pictureDirectoryPath = pictureDirectory.getPath();
-        Uri data = Uri.parse(pictureDirectoryPath);
-
-
-        photoPickerIntent.setDataAndType(data, "image/*");
-
-        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
-
+    public void onGallerySelect(View view) {
+        Intent intent = new Intent(this, galleryActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK)  {
-            if (requestCode == IMAGE_GALLERY_REQUEST) {
-                Uri imageUri = data.getData();
-                InputStream inputStream;;
-
-                try {
-                    inputStream = getContentResolver().openInputStream(imageUri);
-//                    byte[] inputData = getBytes(inputStream);
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    int[][] rgbValues = getRGBValues(bitmap);
-                    ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
-
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "unable to open image", Toast.LENGTH_LONG).show();
-                }
-//                catch (IOException e) {
-//                    e.printStackTrace();
-//                    Toast.makeText(this, "unable to read image", Toast.LENGTH_LONG).show();
-//                }
-            }
-        }
+    public void onPaintSelect(View view) {
+        Intent intent = new Intent(this, paintActivity.class);
+        startActivity(intent);
     }
 
-    public int[][] getRGBValues(Bitmap bmp) {
-
-        int [][] rgbValues = new int[bmp.getWidth()][bmp.getHeight()];
-
-        //Print in LogCat's console each of one the RGB and alpha values from the 4 corners of the image
-        //Top Left
-        Log.i("Pixel Value", "Top Left pixel: " + Integer.toHexString(bmp.getPixel(0, 0)));
-        //Top Right
-        Log.i("Pixel Value", "Top Right pixel: " + Integer.toHexString(bmp.getPixel(31, 0)));
-        //Bottom Left
-        Log.i("Pixel Value", "Bottom Left pixel: " + Integer.toHexString(bmp.getPixel(0, 31)));
-        //Bottom Right
-        Log.i("Pixel Value", "Bottom Right pixel: " + Integer.toHexString(bmp.getPixel(31, 31)));
-
-        //get the ARGB value from each pixel of the image and store it into the array
-        for(int i=0; i < bmp.getWidth(); i++)
-        {
-            for(int j=0; j < bmp.getHeight(); j++)
-            {
-                //This is a great opportunity to filter the ARGB values
-                rgbValues[i][j] = bmp.getPixel(i, j);
-            }
-        }
-        return rgbValues;
-    }
 }
